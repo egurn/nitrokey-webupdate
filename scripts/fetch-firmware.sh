3b9c5681e4bdc930009e5640f62be2c6708fc2f8
Szczepan Zalega <szczepan@nitrokey.com>
2019-09-04 16:24:37 +0200

    Set correct sha names

--- scripts/fetch-firmware.sh
+++ scripts/fetch-firmware.sh
@@ -8,18 +8,21 @@ wget -P data/ ${STABLEURL}
 STABLE_VERSION="$(cat data/STABLE_VERSION)"
 echo "${STABLE_VERSION}"
 
-BINMAIN=fido2-firmware-${STABLE_VERSION}.json
-BINDEV=fido2-firmware-${STABLE_VERSION}.hex
+NAMEMAIN=fido2-firmware-${STABLE_VERSION}
+NAMEDEV=fido2-firmware-dev-${STABLE_VERSION}
+
+BINMAIN=${NAMEMAIN}.json
+BINDEV=${NAMEDEV}.hex
 
 BINURL=https://github.com/Nitrokey/nitrokey-fido2-firmware/releases/download/${STABLE_VERSION}/fido2-firmware-${STABLE_VERSION}
 BINURLDEV=https://github.com/Nitrokey/nitrokey-fido2-firmware/releases/download/${STABLE_VERSION}/fido2-firmware-dev-${STABLE_VERSION}
 
 wget -P data/ ${BINURL}.json
-wget -P data/ ${BINURL}.sha2
+wget -P data/ ${BINURL}.json.sha2
 wget -P data/ ${BINURLDEV}.hex
-wget -P data/ ${BINURLDEV}.sha2
+wget -P data/ ${BINURLDEV}.hex.sha2
 
 pushd data/
-sha256sum -c ${BINMAIN}
-sha256sum -c ${BINDEV}
+sha256sum -c ${BINMAIN}.sha2
+sha256sum -c ${BINDEV}.sha2
 popd
