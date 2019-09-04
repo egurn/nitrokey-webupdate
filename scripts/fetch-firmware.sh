8372d4f5b1ad85bf034f4c28f7d919b7be43aff6
Szczepan Zalega <szczepan@nitrokey.com>
2019-09-04 20:15:04 +0200

    Always use verifying bootloader

--- scripts/fetch-firmware.sh
+++ scripts/fetch-firmware.sh
@@ -12,17 +12,19 @@ NAMEMAIN=fido2-firmware-${STABLE_VERSION}
 NAMEDEV=fido2-firmware-dev-${STABLE_VERSION}
 
 BINMAIN=${NAMEMAIN}.json
-BINDEV=${NAMEDEV}.hex
+BINDEV=${NAMEDEV}.json
 
 BINURL=https://github.com/Nitrokey/nitrokey-fido2-firmware/releases/download/${STABLE_VERSION}/fido2-firmware-${STABLE_VERSION}
 BINURLDEV=https://github.com/Nitrokey/nitrokey-fido2-firmware/releases/download/${STABLE_VERSION}/fido2-firmware-dev-${STABLE_VERSION}
 
 wget -P data/ ${BINURL}.json
 wget -P data/ ${BINURL}.json.sha2
-wget -P data/ ${BINURLDEV}.hex
-wget -P data/ ${BINURLDEV}.hex.sha2
+wget -P data/ ${BINURLDEV}.json
+wget -P data/ ${BINURLDEV}.json.sha2
 
 pushd data/
 sha256sum -c ${BINMAIN}.sha2
 sha256sum -c ${BINDEV}.sha2
 popd
+
+ls -lh data
